bb07f2addb527c555bb617af027f527e1aa64a30
Godfred Amoah Sefa <godfredasefa@icloud.com>
2026-06-20 14:42:19 +0200

    new component

--- untitled/dist/script.js
+++
@@ -1,8 +0,0 @@
-const logos = document.querySelectorAll('div');
-for (let i = 0; i < logos.length; i++) {
-  logos[i].style.setProperty('--i', i);
-  logos[i].style.setProperty('--d', Math.random() * 8);
-  logos[i].style.setProperty('--a', Math.random() * 8 + 4);
-  logos[i].style.setProperty('--hue', Math.floor(Math.random() * 360));
-  logos[i].style.setProperty('--y', Math.floor(Math.random() * 100));
-}
